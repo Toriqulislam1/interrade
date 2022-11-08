b6b41099b15cef1a6621fc032845e4d7c8846c9f
shahin reza <mdshahinreza2041@gmail.com>
2022-11-08 18:33:36 +0600

    error
    error

--- resources/views/frontend/services/child_view.blade.php
+++ resources/views/frontend/services/child_view.blade.php
@@ -12,14 +12,8 @@
 		@endphp
 
 		
-		@php
-		$subcategories = App\Models\subcategory::where('category_id',$category->id)->orderBy('subcategory_name','DESC')->get();
-		@endphp
 
-		@php
-		$childcategories = App\Models\Childcategory::where('subcategory_id',$subcategory->id)->orderBy('childcategory_name','DESC')->get();
-		@endphp
-
+		
 			@foreach($services as $item)
 				<div class="col-lg-3 col-sm-6 mt30 wow fadeInUp" data-wow-delay=".6s">
 					<div class="s-block up-hor pt20">
